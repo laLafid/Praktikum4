144c65333e13b071e804e241e032a0aea9b4c557
laLafid <dzaky878alhafidz@gmail.com>
2024-11-14 04:28:45 +0700

    new file:   README.md 	new file:   program.py p

---
+++ README.md
@@ -0,0 +1,8 @@
+Program Penambah Data
+
+Cara Kerja Program Ini
+1. Mulai
+2. Meminta input
+3. mndata input
+4. berikan output
+
---
+++ program.py
@@ -0,0 +1,34 @@
+def nilai(str):
+    while True:
+        try:
+            return float(input(str))
+        except ValueError:
+            print("Input harus angka!!")
+r = []
+
+while True:
+    Nama = input("Masukkan Nama: ")
+    NIM = int(nilai("Masukkan NIM (e.g. 123456789): "))
+    Tugas = nilai("Masukkan Nilai Tugas: ")
+    UTS = nilai("Masukkan Nilai UTS: ")
+    UAS = nilai("Masukkan Nilai UAS: ")
+    Akhir = ((Tugas*0.3) + (UTS*0.35) + (UAS*0.35))
+    r.append({
+        "Nama": Nama, 
+        "NIM": NIM, 
+        "Nilai Tugas": Tugas, 
+        "Nilai UTS": UTS, 
+        "Nilai UAS": UAS, 
+        "Nilai Akhir": Akhir
+        })
+    ulang = input("Tambah data lagi? (y/t): ").lower()
+    if ulang == "t":
+        break
+    
+
+print('_'*70)
+print(f"| {'No':^3} | {'Nama':^10} | {'NIM':^10} | {'Tugas':^6} | {'UTS':^6} | {'UAS':^6} | {'Akhir':^6} |")
+print('='*70)
+for i, data in enumerate(r, start=1):
+    print(f"| {i:^3} | {data['Nama']:^10} | {data['NIM']:^10} | {data['Nilai Tugas']:^6.1f} | {data['Nilai UTS']:^6.1f} | {data['Nilai UAS']:^6.1f} | {data['Nilai Akhir']:^6.1f} |")
+print('‾'*70)
