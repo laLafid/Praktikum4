2cda949b06074a2a9d89ec3620cd010a5ef208f2
laLafid <dzaky878alhafidz@gmail.com>
2024-11-14 04:46:00 +0700

    modified:   README.md 	new file:   gambar/def.png 	new file:   gambar/minta.png 	new file:   gambar/r.png 	modified:   program.py p

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-Program Penambah Data
+# Program Input Data
 
-Cara Kerja Program Ini
-1. Mulai
-2. Meminta input
-3. mndata input
-4. berikan output
+# Cara Kerja Program Ini
+1. Untuk memastikan bahwa input adalah angka desimal atau float, program mendefinisikan fungsi ```nilai(str)```.
+2. Program membuat list kosong ```r``` untuk menyimpan data.
+3. Dalam loop utama, program meminta nama pengguna, NIM, nilai tugas, nilai UTS, dan nilai UAS. 
+4. Program kemudian menghitung nilai akhir berdasarkan bobot nilai: tugas 30%, UTS 35%, dan UAS 35%. 
+5. Data yang telah dimasukkan disimpan kedalam ```r```, dan pengguna diminta untuk menentukan apakah mereka ingin menambah data lagi. Loop berhenti jika input adalah (t).
+6. Setelah loop selesai, program mencetak tabel yang berisi data yang telah dimasukan, termasuk nilai akhir.
+
 
--- program.py
+++ program.py
@@ -1,18 +1,22 @@
+# fungsi pencegahan errorr
 def nilai(str):
     while True:
         try:
             return float(input(str))
         except ValueError:
             print("Input harus angka!!")
+
+# variable yang kosong
 r = []
 
+# perulangan untuk meminta input dari pengguna
 while True:
-    Nama = input("Masukkan Nama: ")
-    NIM = int(nilai("Masukkan NIM (e.g. 123456789): "))
-    Tugas = nilai("Masukkan Nilai Tugas: ")
-    UTS = nilai("Masukkan Nilai UTS: ")
-    UAS = nilai("Masukkan Nilai UAS: ")
-    Akhir = ((Tugas*0.3) + (UTS*0.35) + (UAS*0.35))
+    Nama     = input("Masukkan Nama: ")
+    NIM      = int(nilai("Masukkan NIM (e.g. 123456789): "))
+    Tugas    = nilai("Masukkan Nilai Tugas: ")
+    UTS      = nilai("Masukkan Nilai UTS: ")
+    UAS      = nilai("Masukkan Nilai UAS: ")
+    Akhir    = ((Tugas*0.3) + (UTS*0.35) + (UAS*0.35))
     r.append({
         "Nama": Nama, 
         "NIM": NIM, 
@@ -21,11 +25,13 @@ while True:
         "Nilai UAS": UAS, 
         "Nilai Akhir": Akhir
         })
-    ulang = input("Tambah data lagi? (y/t): ").lower()
+    # menanyakan untuk menambah data, apabila menerima inout "t" maka program akan menampilkan data
+    ulang    = input("Tambah data lagi? (y/t): ").lower()
     if ulang == "t":
         break
     
 
+# untuk mencetak table data
 print('_'*70)
 print(f"| {'No':^3} | {'Nama':^10} | {'NIM':^10} | {'Tugas':^6} | {'UTS':^6} | {'UAS':^6} | {'Akhir':^6} |")
 print('='*70)
